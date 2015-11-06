d684f10ebd3ab9bfb2b672ccd2c718d0414063b1
Koichi Shiraishi <zchee.io@gmail.com>
2015-11-06 19:55:29 +0900

    Cache SDKs dir & docker pull before build
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- script/build-circleci.sh
+++ script/build-circleci.sh
@@ -6,8 +6,8 @@ then
   docker load < ~/cache/$CIRCLE_PROJECT_REPONAME.tar
 else
   mkdir -p ~/cache
-  sudo lxc-attach -n "$(docker build -t $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME -f $DOCKERFILE .)"
-  md5sum Dockerfile | cut -d' ' -f1 > ~/cache/dockerfile.digest
+  docker build -t $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME -f $DOCKERFILE .
+  md5sum $DOCKERFILE | cut -d' ' -f1 > ~/cache/dockerfile.digest
   docker save $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME > ~/cache/$CIRCLE_PROJECT_REPONAME.tar
 fi
 
